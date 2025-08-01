8b0ab44a43a96fb4d41c30e7ff1d2138ccd2e80d
vishal11u <shitolevishal29@gmail.com>
2025-08-01 00:02:04 -0700

    chore: update package.json to add Jest and testing dependencies, configure Jest, and create initial test for Home page

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../app/page';
+
+describe('Home page', () => {
+  it('renders welcome message', () => {
+    render(<Home />);
+    expect(screen.getByText(/welcome/i)).toBeInTheDocument();
+  });
+});
